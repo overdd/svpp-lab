7b7a2cf0708169dc3003b08cc0e8a00cf5fe28a0
Anton Siarko <anton_siarko@epam.com>
2021-01-05 13:51:20 +0300

    update: Lab 2 is ready (button runs away from mouse pointer)

---
+++ SVPP_Lab_2/SVPP_Lab_2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SVPP_Lab_2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Run(object sender, MouseEventArgs e)
+        {
+            Random randomValue = new Random();
+            NoButton.SetValue(Grid.RowProperty, randomValue.Next(0, 6));
+            NoButton.SetValue(Grid.ColumnProperty, randomValue.Next(0, 6));
+        }
+
+        private void Button_MessageBox(object sender, EventArgs e)
+        {
+            MessageBox.Show("Okay!");
+        }
+    }
+}
