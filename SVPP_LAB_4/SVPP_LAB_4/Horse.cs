e0d089e6bb2d2dfdc7115bad8f60deb98111da55
Anton Siarko <anton_siarko@epam.com>
2021-01-09 23:36:06 +0300

    update: Lab 4 visual part ready

---
+++ SVPP_LAB_4/SVPP_LAB_4/Horse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVPP_LAB_4
+{
+    class Horse
+    {
+        string name;
+        int speed;
+
+        public Horse(string name, int speed)
+        {
+            this.name = name;
+            this.speed = speed;
+        }
+
+        public void Move() {
+
+        }
+    }
+}
