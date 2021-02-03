4ce6fb21ce1730c6d91fc74f6cd55aeac407e26f
Anton Siarko <anton_siarko@epam.com>
2021-02-03 22:25:03 +0300

    update: Lab 7 is ready

---
+++ SVPP_Lab_7/SVPP_Lab_7/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Collections.ObjectModel;
+
+namespace SVPP_Lab_7
+{
+    public partial class MainWindow : Window
+    {
+        readonly ObservableCollection<Railway> Railways;
+        public MainWindow()
+        {
+            Railways = new ObservableCollection<Railway>();
+            InitializeComponent();
+            lBox.DataContext = Railways;
+        }
+        void FillData()
+        {
+            Railways.Clear();
+            foreach (var item in Railway.GetAllRailways())
+            {
+                Railways.Add(item);
+            }
+        }
+        private void ButtonFillClick(object sender, RoutedEventArgs e)
+        {
+            FillData();
+        }
+        private void ButtonAddClick(object sender, RoutedEventArgs e)
+        {
+            var railway = new Railway()
+            {
+                Name = "British Royal Railways",
+                TrainQuantity = 50,
+                RailwayLength = 7500,
+                StaffNumber = 120
+            };
+            railway.Insert();
+            FillData();
+        }
+
+        private void ButtonEditClick(object sender, RoutedEventArgs e)
+        {
+            var railway = (Railway)lBox.SelectedItem;
+            railway.Name = "British renamed railways";
+            railway.Update();
+            FillData();
+        }
+        private void ButtonDeleteClick(object sender, RoutedEventArgs e)
+        {
+            var id = ((Railway)lBox.SelectedItem).Id; Railway.Delete(id);
+            FillData();
+        }
+    }
+}
---
+++ SVPP_Lab_7/SVPP_Lab_7/Railway.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace SVPP_Lab_7
+{
+    class Railway
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TrainQuantity { get; set; }
+        public int RailwayLength { get; set; }
+        public int StaffNumber { get; set; }
+
+        static SqlConnection connection;
+        public Railway()
+        {
+            var connString = ConfigurationManager.ConnectionStrings["DemoConnection"].ConnectionString;
+            connection = new SqlConnection(connString);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("id: {0}, name: {1}, trains: {2}, railways length: {3}, employees: {4}",
+           Id, Name, TrainQuantity, RailwayLength, StaffNumber);
+        }
+        public static IEnumerable<Railway> GetAllRailways()
+        {
+            var commandString = "SELECT Id, Name, TrainQuantity, RailwayLength, StaffNumber FROM [Table]";
+            SqlCommand getAllCommand = new SqlCommand(commandString, connection);
+            connection.Open();
+            var reader = getAllCommand.ExecuteReader(); if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    var id = reader.GetInt32(0);
+                    var name = reader.GetSqlString(1);
+                    var trainQuantity = reader.GetInt32(2);
+                    var railwayLength = reader.GetInt32(3);
+                    var staffNumber = reader.GetInt32(4);
+                    var railway = new Railway
+                    {
+                        Id = id,
+                        Name = name.IsNull == true ? "No name" : name.Value,
+                        TrainQuantity = trainQuantity,
+                        RailwayLength = railwayLength,
+                        StaffNumber = staffNumber
+                    };
+                    yield return railway;
+                }
+            };
+            connection.Close();
+        }
+
+        public void Insert()
+        {
+            var commandString = "INSERT INTO [Table] (Name, TrainQuantity,  RailwayLength, StaffNumber)" + "VALUES (@name, @trainQuantity, @railwayLength, @staffNumber)"; SqlCommand insertCommand = new SqlCommand(commandString, connection);
+            insertCommand.Parameters.AddRange(new SqlParameter[]
+            {
+                    new SqlParameter("name", Name),
+                    new SqlParameter("trainQuantity", TrainQuantity),
+                    new SqlParameter("railwayLength", RailwayLength),
+                    new SqlParameter("staffNumber", StaffNumber)
+            });
+            connection.Open();
+            insertCommand.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public static Railway GetRailway(int id)
+        {
+            foreach (var rw in GetAllRailways())
+            {
+                if (rw.Id == id)
+                    return rw;
+            }
+            return null;
+        }
+
+        public void Update()
+        {
+            var commandString = "UPDATE [Table] SET Name=@name,  TrainQuantity=@trainQuantity, RailwayLength=@railwayLength, StaffNumber=@staffNumber WHERE(Id=@id)"; SqlCommand updateCommand = new SqlCommand(commandString, connection);
+            updateCommand.Parameters.AddRange(new SqlParameter[] { 
+                new SqlParameter("name", Name),
+                new SqlParameter("trainQuantity", TrainQuantity),
+                new SqlParameter("railwayLength", RailwayLength),
+                new SqlParameter("staffNumber", StaffNumber),
+                new SqlParameter("Id", Id),  });
+            connection.Open();
+            updateCommand.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public static void Delete(int id)
+        {
+            var commandString = "DELETE FROM [Table]  WHERE(Id=@id)";
+            SqlCommand deleteCommand = new SqlCommand(commandString, connection);
+            deleteCommand.Parameters.AddWithValue("id", id);
+            connection.Open();
+            deleteCommand.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+}
