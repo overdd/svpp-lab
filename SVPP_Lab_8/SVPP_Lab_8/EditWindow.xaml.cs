9e093773f15f391670eb83a5443dba9511cc00c8
Anton Siarko <anton_siarko@epam.com>
2021-02-03 23:25:20 +0300

    update: Laba 8 is ready

---
+++ SVPP_Lab_8/SVPP_Lab_8/EditWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+
+namespace SVPP_Lab_8
+{
+    public partial class EditWindow : Window
+    {
+        readonly Railway railway;
+        public EditWindow()
+        {
+            InitializeComponent();
+        }
+
+        public EditWindow(Railway railway) : this()
+        {
+            this.railway = railway;
+            grid.DataContext = railway;
+        }
+
+        private void ButtonOkClick(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void ButtonCancelClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ SVPP_Lab_8/SVPP_Lab_8/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System.Data.Entity;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SVPP_Lab_8
+{
+    public partial class MainWindow : Window
+    {
+        EntityContext context;
+        public MainWindow()
+        {
+            context = new EntityContext("TestDbConnection");
+            InitializeComponent();
+            context.railways.Load();
+            dataGrid.DataContext = context.railways.Local;
+        }
+
+        private void ButtonAddClick(object sender, RoutedEventArgs e)
+        {
+            var railway = new Railway();
+            EditWindow ew = new EditWindow(railway);
+            var result = ew.ShowDialog();
+            if (result == true)
+            {
+                context.Zkhs.Add(railway);
+                context.SaveChanges();
+                ew.Close();
+            }
+        }
+
+        private void ButtonEditClick(object sender, RoutedEventArgs e)
+        {
+            Railway railway = dataGrid.SelectedItem as Railway;
+            EditWindow ew = new EditWindow(railway);
+            var result = ew.ShowDialog();
+            if (result == true)
+            {
+                context.SaveChanges();
+                ew.Close();
+            }
+            else
+            {
+                context.Entry(railway).Reload();
+                dataGrid.DataContext = null;
+                dataGrid.DataContext = context.Zkhs.Local;
+            }
+        }
+        private void ButtonDeleteClick(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Are you sure?", "Deleting record...", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                Railway railway = dataGrid.SelectedItem as Railway;
+                context.Railways.Remove(railway);
+                context.SaveChanges();
+            }
+        }
+    }
+}
